ee88f997f094084e71dd3ceb1eadaf481ad1a330
Tenal <tenalbourchier@gmail.com>
2020-12-31 10:47:54 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="wrapper">
-                <p>&#169; Tenal Bourchier 2020 (made at <a href="https://junocollege.com/">Juno College</a>)</p>
+                <p>&#169; Tenal Bourchier 2020</p>
                 <p>Photography &#169; Tenal & Jaden Bourchier</p>
             </div>
         </footer>
